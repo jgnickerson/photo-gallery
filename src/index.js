10d72f3f85de086eb65f2697cfc23b668c44de64
jgnickerson <jgnickerson@me.com>
2017-06-11 20:32:46 -1000

    basic SPA functionality

--- src/index.js
+++ src/index.js
@@ -9,9 +9,32 @@ import './index.css';
 const storageRef = storage.ref();
 const images = storageRef.child('images');
 
-//createImageJson();
+window.addEventListener("hashchange", ()=>{
+  render(decodeURI(window.location.hash));
+});
 
 //fetches metadata and initializes grid
 db.ref('imageMetadata').once("value").then(result=> {
   var grid = new Grid("grid", result.val());
+
+  window.dispatchEvent(new HashChangeEvent('hashchange'));
 });
+
+
+
+function render(url) {
+  const keyword = url.split('/')[0];
+
+  const visible = document.querySelector(".page-content .page.visible");
+  if (visible)
+    visible.classList.remove("visible");
+
+  switch (keyword) {
+    case "#admin" :
+      document.querySelector(".page.admin").classList.add("visible");
+      break;
+
+    default:
+      document.querySelector(".page.grid").classList.add("visible");
+  }
+}
